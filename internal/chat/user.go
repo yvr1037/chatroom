b11dbd490d2ae5c400803811944a0662310bb1c8
yvr <vrshadow20508@gmail.com>
2023-04-19 18:35:09 +0800

    websocket receive json

--- internal/chat/user.go
+++ internal/chat/user.go
@@ -48,7 +48,8 @@ func (u *User) CloseMessageChannel() {
 
 func (u *User) ReceiveMessage(ctx context.Context) error {
 	var (
-		receiveMessage string
+		// receiveMessage string
+		receiveMessage map[string]string
 		err            error
 	)
 	for {
@@ -64,7 +65,7 @@ func (u *User) ReceiveMessage(ctx context.Context) error {
 			return err
 		}
 
-		sendMessage := NewMessage(u,receiveMessage)
+		sendMessage := NewMessage(u,receiveMessage["message_content"])
 		Broadcaster.Broadcast(sendMessage)
 	}
 }
